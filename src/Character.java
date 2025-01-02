dcdc54334b02af98c3d4059d24333cd6edf7303d
dejan-marlovic <dmarlovic83@gmail.com>
2025-01-02 12:28:07 +0100

    started a project

---
+++ src/Character.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+abstract class Character {
+
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    public ArrayList<Item> inventory = new ArrayList<Item>();
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+
+    public abstract void attack();
+
+    public abstract void defend();
+
+    public void useItem(Item item){
+        inventory.add(item);
+    }
+
+}
